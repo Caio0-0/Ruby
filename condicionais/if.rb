9124026b2671a4bde6135ee45fa8dbcfc3fac097
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-08-13 17:33:39 -0300

    new code

---
+++ condicionais/if.rb
@@ -0,0 +1,6 @@
+x=10
+
+if x == 10 
+  puts "voce e gay" 
+
+end
---
+++ numero.rb
@@ -0,0 +1,15 @@
+puts "incira um numero"
+x = gets.chomp.to_i
+puts "icira outro numero"
+y = gets.chomp.to_i
+
+soma = x+y
+subt = x-y
+mult = x*y
+div = x/y
+
+puts "#{x}+#{y}=#{soma}"
+puts "#{x}-#{y}=#{subt}"
+puts "#{x}*#{y}=#{mult}"
+puts "#{x}/#{y}=#{div}"
+
